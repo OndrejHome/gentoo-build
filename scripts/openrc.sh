308f6464d19ecf328b86831278996bbdf13c1265
Alexander Kurakin <kuraga333@mail.ru>
2020-05-23 10:18:09 +0300

    OpenRC build: merge world

--- scripts/openrc.sh
+++ scripts/openrc.sh
@@ -6,4 +6,5 @@ chroot "${GB_ROOT}" /bin/bash -x -e <<-'EOF'
 eselect profile set "${GB_ESELECT_PROFILE}"
 emerge --noreplace app-admin/sysklogd
 ln -s /etc/init.d/sysklogd /etc/runlevels/default
+emerge -vDN @world
 EOF
