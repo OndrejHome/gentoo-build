2a2b52cf25a5d38b7f399072c591b45404f8db47
Ondrej Famera <ondrej@famera.cz>
2020-10-12 17:53:49 +0900

    FIX: add locate while in chroot

--- scripts/locale.sh
+++ scripts/locale.sh
@@ -2,12 +2,12 @@
 set -x
 set -e
 
-echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
 chroot ${GB_ROOT} /bin/bash <<-'EOF'
 source /etc/profile
 set -x
 set -e
 
+echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
 locale-gen
 eselect locale set en_US.UTF-8
 env-update
