f3bd65a545619fa12e2d1592fc7c5310a069ee5f
Alexander Kurakin <kuraga333@mail.ru>
2022-08-28 21:06:03 +0300

    Mount /run
    See https://wiki.gentoo.org/wiki/Handbook:AMD64/Installation/Base#Mounting_the_necessary_filesystems

--- scripts/prepare-chroot.sh
+++ scripts/prepare-chroot.sh
@@ -16,6 +16,11 @@ if ! cat /proc/mounts | cut -d' ' -f 2 | grep -q "^${GB_ROOT}/dev$"; then
   mount --make-rslave ${GB_ROOT}/dev
 fi
 
+if ! cat /proc/mounts | cut -d' ' -f 2 | grep -q "^${GB_ROOT}/run$"; then
+  mount --bind /run ${GB_ROOT}/run
+  mount --make-slave ${GB_ROOT}/run
+fi
+
 if [ -L /dev/shm ]; then
   rm /dev/shm
   mkdir /dev/shm
