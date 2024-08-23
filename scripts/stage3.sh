41a67464e601ebc8b1529b43fb5bcd0bbe21ce59
Ondrej Famera <ondrej@famera.cz>
2024-08-23 10:35:19 +0900

    support build on both x86_64 and aarch64 - detect aarch64 and download appropriate stage3 instead of hardcoded amd64

--- scripts/stage3.sh
+++ scripts/stage3.sh
@@ -7,17 +7,22 @@ if [ -e "${GB_ROOT}/stage3.tar.xz" ] ; then
   exit 0
 fi
 
+ARCH='amd64'
+if [ $(uname -m) == 'aarch64' ]; then
+  ARCH='arm64'
+fi
+
 if [ "$GB_INIT" = "systemd" ]; then
   if [ "${GB_STAGE3}" = "latest" ]; then
-    wget -O "${GB_ROOT}/stage3.tar.xz" "${GB_STAGE3_MIRROR}"'/releases/amd64/autobuilds/current-stage3-amd64-systemd/stage3-amd64-systemd-2*.tar.xz'
+    wget -O "${GB_ROOT}/stage3.tar.xz" "${GB_STAGE3_MIRROR}/releases/${ARCH}/autobuilds/current-stage3-${ARCH}-systemd/stage3-${ARCH}-systemd-2*.tar.xz"
   else
-    wget -O "${GB_ROOT}/stage3.tar.xz" "${GB_STAGE3_MIRROR}/releases/amd64/autobuilds/${GB_STAGE3}/stage3-amd64-systemd-${GB_STAGE3}.tar.xz"
+    wget -O "${GB_ROOT}/stage3.tar.xz" "${GB_STAGE3_MIRROR}/releases/${ARCH}/autobuilds/${GB_STAGE3}/stage3-${ARCH}-systemd-${GB_STAGE3}.tar.xz"
   fi
 else
   if [ "${GB_STAGE3}" = "latest" ]; then
-    wget -O "${GB_ROOT}/stage3.tar.xz" "${GB_STAGE3_MIRROR}"'/releases/amd64/autobuilds/current-stage3-amd64-openrc/stage3-amd64-openrc-2*.tar.xz'
+    wget -O "${GB_ROOT}/stage3.tar.xz" "${GB_STAGE3_MIRROR}/releases/${ARCH}/autobuilds/current-stage3-${ARCH}-openrc/stage3-${ARCH}-openrc-2*.tar.xz"
   else
-    wget -O "${GB_ROOT}/stage3.tar.xz" "${GB_STAGE3_MIRROR}/releases/amd64/autobuilds/${GB_STAGE3}/stage3-amd64-openrc-${GB_STAGE3}.tar.xz"
+    wget -O "${GB_ROOT}/stage3.tar.xz" "${GB_STAGE3_MIRROR}/releases/${ARCH}/autobuilds/${GB_STAGE3}/stage3-${ARCH}-openrc-${GB_STAGE3}.tar.xz"
   fi
 
 fi
